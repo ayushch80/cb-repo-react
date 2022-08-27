4232dfd6b0418129c5c50422a1079117f511eff3
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-08-27 18:12:21 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as Bootstrap from "bootstrap";
 <nav class="navbar navbar-dark navbar-expand-md bg-dark py-3" style="background: rgb(0,0,0);">
         <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon" style="background: rgb(230,194,6);"><i class="fas fa-fire"></i></span><span style="font-family: Arbutus, serif;">COIN BURN</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-5">
