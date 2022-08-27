565b2f5f6ea471d089ce3bb3e04872177c4554de
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-08-27 18:01:15 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { RNNSearchBar } from "react-native-navigation-search-bar";
 <RNNSearchBar
-    componentId={componentId} // <-- RNN component id
-    statusBarHeight={statusBarHeight} // <-- prop status bar height
-    search={...}
-    onChangeText={...}
+    componentId="hi" // <-- RNN component id
+    statusBarHeight=10 // <-- prop status bar height
+    search="hello"
+    onChangeText="okay"
     
  />
