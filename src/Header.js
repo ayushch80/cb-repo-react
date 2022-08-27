29567a23ef0901086ff753af22ccee8592f7ceae
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-08-27 18:11:42 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { RNNSearchBar } from "react-native-navigation-search-bar";
-<RNNSearchBar
-    componentId="hi" // <-- RNN component id
-    statusBarHeight=10 // <-- prop status bar height
-    search="hello"
-    onChangeText="okay"
-    
- />
+<nav class="navbar navbar-dark navbar-expand-md bg-dark py-3" style="background: rgb(0,0,0);">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon" style="background: rgb(230,194,6);"><i class="fas fa-fire"></i></span><span style="font-family: Arbutus, serif;">COIN BURN</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-5">
+                <ul class="navbar-nav ms-auto"></ul><a class="btn btn-primary ms-md-2" role="button" href="#" style="background: rgb(48,117,222);font-weight: bold;text-align: center;font-size: 15px;color: rgb(204,182,182);" id="conn">CONNECT WALLET</a>
+            </div>
+        </div>
+</nav>
